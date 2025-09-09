48bed8293a1abbb16e46955fbe7afa7057908e1a
Test <testgyaan12@gmail.com>
2025-09-09 20:04:25 +0000

    Try fixing this error: `Console Error: Error: Cannot destructure propert

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -45,7 +45,8 @@ export default function Chatbot() {
     setInput('');
 
     try {
-      const { answer } = await chatAboutPortfolio(question);
+      const result = await chatAboutPortfolio(question);
+      const answer = result.answer;
       return {
         messages: [
           ...newMessages,
