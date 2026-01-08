ababf8224cd99122e14d5c0a5ea0fc62cc2a1c39
Test <testgyaan12@gmail.com>
2026-01-08 16:30:28 +0000

    Try fixing this error: `Runtime Error: Error: cn is not defined. Error s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,6 +62,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { cn } from '@/lib/utils';
 
 type Product = {
   id: number;
@@ -1023,6 +1024,9 @@ export default function Home() {
     
 
     
+
+
+
 
 
 
