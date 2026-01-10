998e80b7374c66e3e279e6cd588cac7a364a1018
Test <testgyaan12@gmail.com>
2026-01-10 15:02:23 +0000

    cloadnary image

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
